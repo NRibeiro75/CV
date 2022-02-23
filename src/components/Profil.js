c6faa1948e945d635920e51e4e7255c6bc6fcdfd
Ribeiro <nicolasribeiro75@gmail.com>
2022-02-23 15:32:45 +0100

    ajout du profil

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -5,11 +5,12 @@ function Profil() {
     <div className="profil mb5">
         <h2 className="h2">Profil</h2>
         <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem eum exercitationem voluptates nisi ipsam, explicabo cumque illum tempore iusto corporis fugit reprehenderit id est dolores aperiam excepturi? Sint, ipsum suscipit.
-            Ducimus temporibus quae animi eveniet natus, ullam rem fugiat nam pariatur officiis! Libero incidunt nihil ratione rem numquam et dolor culpa provident sint, velit iusto, dolore eaque nobis, dicta at.
-            Ad nulla temporibus reprehenderit, natus minima enim repellendus omnis neque a itaque, sed odio accusantium eos asperiores quis perferendis debitis quo quaerat vel ipsa? Fugiat ut cumque labore earum! Eius.
-            Praesentium animi cumque sint in deleniti reiciendis iure unde dolore voluptate aperiam quis fugiat iste dolores, odio obcaecati dolorum veniam sit officia magni nam deserunt. Consectetur eaque similique harum aliquam.
-            Expedita, sint. Iure delectus libero velit quisquam dolore hic sapiente autem a omnis laboriosam pariatur veritatis sint cupiditate unde culpa officiis ipsum assumenda nostrum excepturi, sequi odio ad numquam eveniet?
+        Je suis d’origine franco-portugaise, passionné de voyages, de musique et de sneakers.<br /> 
+        J’ai longtemps évolué dans les secteurs de la restauration et de l’événementiel avant d’entamer une reconversion professionnelle vers le milieu du numérique qui m’intéresse depuis de nombreuses années.<br /> 
+        La crise sanitaire que nous traversons depuis deux ans, a été pour moi l’opportunité de repenser mon avenir professionnel.<br /> 
+        Mon naturel dynamique, motivé et curieux sont des atouts pour m’aider à relever tous les défis et particulièrement ceux liés à la reprise de mes études.<br />
+        J’aime travailler en équipe et relever des challenges mais sais également faire preuve d’autonomie.<br />
+        Il me tarde d’intégrer une nouvelle équipe ! 
         </p>
     </div>
   )
