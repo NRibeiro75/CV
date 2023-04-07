0c6dc19e64242d8410766186703eba2bb11bbb9b
RIBEIRO NICOLAS <nicolasribeiro75@gmail.com>
2023-04-07 17:42:17 -0400

    visa add

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -10,7 +10,9 @@ function Profil() {
         I am dynamic, motivated and curious,always willing to embrace a new challenge and learn new skills.<br />
         My experience working in Paris renowned restaurants will be an asset to join your team.<br />
         I am a team player, always wiiling to support my colleagues in order to deliver the best experience and service to the guests.<br />
-        I can't wait to join your team!
+        I can't wait to join your team!<br />
+
+        <strong> I have a Visa and a Social Security Number </strong> 
         
         </p>
     </div>
